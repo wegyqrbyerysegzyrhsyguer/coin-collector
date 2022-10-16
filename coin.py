452f39e60049e70bd29aa63f807e4eaffa59f3dc
Stepan Yashin <53849083+Stepan363@users.noreply.github.com>
2022-10-16 18:55:11 +1300

    Ready version
    READY VERSION IF YOU WANT TO PLAY.
    
    REQUIRES PYTHON

--- coin.py
+++ coin.py
@@ -1,11 +1,15 @@
 # Stepan Yashin proudy made this game, It took a lot of effort, But I had finally converted it into 2 player mode. ENJOY!!! 
 #10/15/22, In airplane, added player 1 && fox. :) ( From Sydney to Auckland.)
-
-
+# WASD PLAYER 1
+# ARROW KEYS PLAYER 2
+# Z & M TO RESET POSITION OF FOX / HEDGEHOG
+import pygame
 from random import randint
 import pgzrun
+import time
 WIDTH = 500
-HEIGHT = 500
+HEIGHT = 450
+time_left = 60
 player_1_score = 0 
 player_2_score = 0
 game_over = False
@@ -16,19 +20,24 @@ hedgehog = Actor("hedgehog")
 hedgehog.pos = 150, 100
 coin = Actor("coin")
 coin.pos = 200, 200
-
+background = pygame.image.load(r'C:\Users\Evgeny\Desktop\Python\Code GAMES\coin collector\images/coins_background.png')
 def draw():
-    screen.fill("green")
+    screen.blit(background, (0, 0))
     fox.draw()
     coin.draw()
     hedgehog.draw()
-    screen.draw.text("hedgehog: " + str(player_1_score), color="black", topleft=(10, 10))
-    screen.draw.text("fox: " + str(player_2_score), color="black", topleft=(10, 30))
+    screen.draw.text("fox: " + str(player_1_score), color="black", topleft=(10, 10))
+    screen.draw.text("hedgehog: " + str(player_2_score), color="black", topleft=(10, 30))
 
     if game_over:
-        screen.fill("pink")
-        screen.draw.text("hedgehog: " + str(player_1_score), topleft=(10, 10), fontsize=60)
-        screen.draw.text("fox: " + str(player_2_score), topleft=(10, 10), fontsize=60)
+        screen.fill("#800080")
+        if (player_1_score > player_2_score):
+            screen.draw.text("FOX WINS, SCORE: " + str(player_1_score), color="orange", topleft=(10, 30))
+        if (player_2_score > player_1_score):
+            screen.draw.text("HEDGEHOG WINS, SCORE: " + str(player_2_score), color="orange", topleft=(10, 30))
+        if (player_1_score == player_2_score):
+            screen.draw.text("TIE!", color="orange", topleft=(10, 30))
+            
 
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
@@ -43,7 +52,6 @@ def time_up():
 def update():
     global player_1_score
     global player_2_score
-
     if keyboard.left:
         fox.x = fox.x - 10
     elif keyboard.right:
@@ -52,6 +60,9 @@ def update():
         fox.y = fox.y - 10
     elif keyboard.down:
         fox.y = fox.y + 10
+    elif keyboard.m:
+        fox.y = 300
+        fox.x = 200
 
     if keyboard.a:
         hedgehog.x = hedgehog.x - 10
@@ -61,6 +72,10 @@ def update():
         hedgehog.y = hedgehog.y - 10
     elif keyboard.s:
         hedgehog.y = hedgehog.y + 10
+    elif keyboard.z:
+        hedgehog.y = 300
+        hedgehog.x = 200
+    
 
     coin_collected = fox.colliderect(coin)
     coin_collected2 = hedgehog.colliderect(coin)
@@ -71,9 +86,13 @@ def update():
         player_2_score = player_2_score + 1
         place_coin()
 
+
+
 clock.schedule(time_up, 60)
 place_coin()
 pgzrun.go()
 
 # WR 2 PLAYER: 2880
 # WR 1 PLAYER: 2400
+
+# TASKS: MAKE HEDGEHOG & FOX GO TO MIDDLE AFTER TOUCHING BORDER.
